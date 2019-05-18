3dda1795cb5401c497eb67ab07c7fd561717b432
DELL <500068596@stu.upes.ac.in>
2019-05-18 18:08:37 +0530

    initial commit

---
+++ app/src/main/java/com/example/frontpage/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.frontpage;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button login, Button, Button2, Button4;
+    ImageButton imageButton2, imageButton5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
